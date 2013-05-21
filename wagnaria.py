9715d5d52b6af4a5ce9312fafddba2f6b2c5fd57
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2013-05-21 09:24:27 -0400

    Validate ObjectId fields in routes.

--- wagnaria.py
+++ wagnaria.py
@@ -22,18 +22,18 @@ def install_routes(app):
     app.route('/', ['ANY'], index)
     app.route('/shows', ['GET'], load_shows)
     app.route('/shows/<group:re:[a-z_]+>', ['GET'], load_shows)
-    app.route('/show/<_id>', ['GET'], load_show)
-    app.route('/show/<_id>/<column:re:[a-z_.]+>', ['GET'], load_show)
+    app.route('/show/<oid:re:[0-9a-f]{24}:re:[0-9a-f]{24}>', ['GET'], load_show)
+    app.route('/show/<oid:re:[0-9a-f]{24}>/<column:re:[a-z_.]+>', ['GET'], load_show)
     app.route('/staff', ['GET'], load_staff)
-    app.route('/staff/<_id>', ['GET'], load_member)
-    app.route('/staff/<_id>/shows', ['GET'], load_members_shows)
+    app.route('/staff/<oid:re:[0-9a-f]{24}>', ['GET'], load_member)
+    app.route('/staff/<oid:re:[0-9a-f]{24}>/shows', ['GET'], load_members_shows)
     app.route('/shows', ['POST'], create_show)
-    app.route('/show/<_id>', ['PUT'], modify_show)
-    app.route('/show/<_id>', ['DELETE'], destroy_show)
-    app.route('/show/<_id>/blame', ['GET'], blame_show)
+    app.route('/show/<oid:re:[0-9a-f]{24}>', ['PUT'], modify_show)
+    app.route('/show/<oid:re:[0-9a-f]{24}>', ['DELETE'], destroy_show)
+    app.route('/show/<oid:re:[0-9a-f]{24}>/blame', ['GET'], blame_show)
     app.route('/staff', ['POST'], create_member)
-    app.route('/staff/<_id>', ['PUT'], modify_member)
-    app.route('/staff/<_id>', ['DELETE'], destroy_member)
+    app.route('/staff/<oid:re:[0-9a-f]{24}>', ['PUT'], modify_member)
+    app.route('/staff/<oid:re:[0-9a-f]{24}>', ['DELETE'], destroy_member)
 
 # Index page
 def index():
@@ -63,24 +63,24 @@ def load_shows(group=None):
     return prepare_json(shows)
 
 # Return a show
-def load_show(_id, column=None):
-    show = [db.shows.find_one({'_id': ObjectId(_id)})]
+def load_show(oid, column=None):
+    show = [db.shows.find_one({'_id': ObjectId(oid)})]
     if not show:
-        raise HTTPError(404, 'There is no show with an ObjectId of "{0}".'.format(_id))
+        raise HTTPError(404, 'There is no show with an ObjectId of "{0}".'.format(oid))
     if not column:
         show = map(lambda s: resolve_staff(s), show)
         return prepare_json(show)
     else:
-        field = db.shows.find_one({'_id': ObjectId(_id)}, {column: 1, '_id': 0})
+        field = db.shows.find_one({'_id': ObjectId(oid)}, {column: 1, '_id': 0})
         if not field:
             raise HTTPError(404, 'The "{0}" field does not exist for {1}'.format(column, show['titles']['english']))
         return prepare_json([field])
 
 # Return a member's information
-def load_member(_id):
-    member = db.staff.find_one({'_id': ObjectId(_id)})
+def load_member(oid):
+    member = db.staff.find_one({'_id': ObjectId(oid)})
     if not member:
-        raise HTTPError(404, 'There is no staff member with an ObjectId of "{0}".'.format(_id))
+        raise HTTPError(404, 'There is no staff member with an ObjectId of "{0}".'.format(oid))
     return prepare_json([member])
 
 # Return a list of all staff
@@ -89,8 +89,8 @@ def load_staff():
     return prepare_json(staff)
 
 # Return a list of shows a staff member has worked on
-def load_members_shows(_id):
-    oid = ObjectId(_id)
+def load_members_shows(oid):
+    oid = ObjectId(oid)
     results = db.shows.find({'$or': [{'staff.translator.id': oid}, {'staff.typesetter.id': oid},
                                  {'staff.timer.id': oid}, {'staff.editor.id': oid}]})
     shows = map(lambda s: s['titles']['english'], results)
@@ -104,23 +104,23 @@ def create_show():
     return show_data
 
 # Update a show's metadata
-def modify_show(_id):
+def modify_show(oid):
     show_data = request.json
     #sanitization, set values for null data
     # show.update({id: id}, {'$set': {show_data}})
     return show_data
 
 # Remove a show from the shows collection
-def destroy_show(_id):
+def destroy_show(oid):
     #check if exists
     # shows.remove({id: id})
     return {'success': True}
 
 # Return information about who's stalling a show
-def blame_show(_id):
+def blame_show(oid):
     #resolve id function here
     # shows.find({id: id})
-    return "Return position and value for whoever is stalling show '{0}'.".format(_id)
+    return "Return position and value for whoever is stalling show '{0}'.".format(oid)
 
 # Insert a new member into the staff collection
 def create_member():
@@ -130,14 +130,14 @@ def create_member():
     return member_data
 
 # Update a member's metadata
-def modify_member(_id):
+def modify_member(oid):
     member_data = request.json
     #sanitization, set values for null data
     # staff.update({id: id}, {'$set': {member_data}})
     return member_data
 
 # Remove a member from the staff collection
-def destroy_member(_id):
+def destroy_member(oid):
     # staff.remove({id: id})
     return {'success': True}
 
