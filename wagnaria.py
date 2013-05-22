ec3d90a017a0b254ea79bf4c73bbe6485632ee18
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2013-05-22 06:31:35 -0400

    PEP8 changes, mostly done I think (comments exempted) [broken]

--- wagnaria.py
+++ wagnaria.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import bottle
-from bottle import HTTPError, request, response
 import yaml
-from pymongo import MongoClient
+import datetime
 import json
+
+from pymongo import MongoClient
 from bson.json_util import dumps
 from bson.objectid import ObjectId
-import datetime
+
+import bottle
+from bottle import HTTPError, request, response
 
 f = open('config.yaml')
 settings = yaml.load(f)
@@ -21,39 +23,58 @@ db = client[settings['mongo']['name']]
 def install_routes(app):
     app.route('/', ['ANY'], index)
     app.route('/shows', ['GET'], load_shows)
-    app.route('/shows/<group:re:[a-z_]+>', ['GET'], load_shows)
-    app.route('/show/<oid:re:[0-9a-f]{24}:re:[0-9a-f]{24}>', ['GET'], load_show)
-    app.route('/show/<oid:re:[0-9a-f]{24}>/<column:re:[a-z_.]+>', ['GET'], load_show)
+    app.route('/shows/<oid:re:[0-9a-f]{24}:re:[0-9a-f]{24}>', ['GET'],
+              load_show)
+    app.route('/shows/<oid:re:[0-9a-f]{24}>/<column:re:[a-z_.]+>', ['GET'],
+              load_show)
+    app.route('/shows/<group:re:[a-z_]+>', ['GET'],
+              load_shows)
+    app.route('/shows', ['POST'], create_show)
+    app.route('/shows/<oid:re:[0-9a-f]{24}>', ['PUT'], modify_show)
+    app.route('/shows/<oid:re:[0-9a-f]{24}>', ['DELETE'], destroy_show)
+    app.route('/shows/<oid:re:[0-9a-f]{24}>/blame', ['GET'], blame_show)
     app.route('/staff', ['GET'], load_staff)
     app.route('/staff/<oid:re:[0-9a-f]{24}>', ['GET'], load_member)
-    app.route('/staff/<oid:re:[0-9a-f]{24}>/shows', ['GET'], load_members_shows)
-    app.route('/shows', ['POST'], create_show)
-    app.route('/show/<oid:re:[0-9a-f]{24}>', ['PUT'], modify_show)
-    app.route('/show/<oid:re:[0-9a-f]{24}>', ['DELETE'], destroy_show)
-    app.route('/show/<oid:re:[0-9a-f]{24}>/blame', ['GET'], blame_show)
+    app.route('/staff/<oid:re:[0-9a-f]{24}>/shows', ['GET'],
+              load_members_shows)
     app.route('/staff', ['POST'], create_member)
     app.route('/staff/<oid:re:[0-9a-f]{24}>', ['PUT'], modify_member)
     app.route('/staff/<oid:re:[0-9a-f]{24}>', ['DELETE'], destroy_member)
 
 # Index page
 def index():
-    return "<pre>Strawberries and cream, \nbiscuits and tea, \nwon't you join me \nin the oak tree?</pre>"
+    return("<pre>Strawberries and cream, \nbiscuits and tea, \nwon't you join "
+           "me \nin the oak tree?</pre>")
+
+class Collection(object):
+    def __init__(self, collection):
+        self.collection = collection
+        self.entries = self.collection.find()
+    def find_by_id(self, oid):
+        return [self.collection.find_one({'_id': ObjectId(oid)})]
+    def find(self, query=None, projection=None):
+        return self.collection.find(query, projection)
 
 def resolve_staff(show):
-    for position in ['translator', 'editor', 'timer', 'typesetter']:
+    for position in ('translator', 'editor', 'timer', 'typesetter'):
         if 'name' not in show['staff'][position]:
-            show['staff'][position]['name'] = db.staff.find_one({'_id': ObjectId(show['staff'][position]['id'])})['name']
+            show['staff'][position]['name'] = db.staff.find_one(
+                {'_id': ObjectId(show['staff'][position]['id'])}
+            )['name']
     return show
 
 # Return a list of shows
 def load_shows(group=None):
     if group:
         query = {
-            'complete': { "status": "complete" },
-            'incomplete': { "status": { "$in": [ "unaired", "airing", "incomplete" ] } },
+            'complete': {"status": "complete"},
+            'incomplete': 
+                {"status": {"$in": ["unaired", "airing", "incomplete"]}},
             'airing': { "status": "airing" },
-            'aired': { "status": "airing", "progress.encoded": False, "airtime": {"$lt": datetime.datetime.utcnow()} },
-            'current_episodes': { "status": "airing", "episodes": { "current": { "$gt": 0 } } }
+            'aired': {"status": "airing", "progress.encoded": False,
+                "airtime": {"$lt": datetime.datetime.utcnow()}},
+            'current_episodes': {"status": "airing",
+                "episodes": {"current": {"$gt": 0}}}
         }.get(group)
         if not query:
             raise HTTPError(404, 'Group "{0}" does not exist.'.format(group))
@@ -67,21 +88,23 @@ def load_shows(group=None):
 def load_show(oid, column=None):
     show = [db.shows.find_one({'_id': ObjectId(oid)})]
     if not show:
-        raise HTTPError(404, 'There is no show with an ObjectId of "{0}".'.format(oid))
+        raise HTTPError(404, 'There is no show with an ObjectId of %s.' % oid)
     if not column:
         show = map(lambda s: resolve_staff(s), show)
         return prepare_json(show)
     else:
-        field = db.shows.find_one({'_id': ObjectId(oid)}, {column: 1, '_id': 0})
+        field = db.shows.find_one({'_id': ObjectId(oid)},
+                                  {column: 1, '_id': 0})
         if not field:
-            raise HTTPError(404, 'The "{0}" field does not exist for {1}'.format(column, show['titles']['english']))
+            raise HTTPError(404, 'The "%s" field does not exist for %s' %
+                            column, show['titles']['english']))
         return prepare_json([field])
 
 # Return a member's information
 def load_member(oid):
     member = db.staff.find_one({'_id': ObjectId(oid)})
     if not member:
-        raise HTTPError(404, 'There is no staff member with an ObjectId of "{0}".'.format(oid))
+        raise HTTPError(404, 'There is no staff with an ObjectId of %s.' % oid)
     return prepare_json([member])
 
 # Return a list of all staff
@@ -92,7 +115,8 @@ def load_staff():
 # Return a list of shows a staff member has worked on
 def load_members_shows(oid):
     oid = ObjectId(oid)
-    results = db.shows.find({'$or': [{'staff.{0}.id'.format(p): oid} for p in ['translator', 'editor', 'timer', 'typesetter']]})
+    results = db.shows.find({'$or': [{'staff.{0}.id'.format(p): oid} for p in 
+        ('translator', 'editor', 'timer', 'typesetter')]})
     shows = map(lambda s: s['titles']['english'], results)
     return prepare_json(shows)
 
@@ -120,7 +144,7 @@ def destroy_show(oid):
 def blame_show(oid):
     #resolve id function here
     # shows.find({id: id})
-    return "Return position and value for whoever is stalling show '{0}'.".format(oid)
+    return "Return whoever is stalling show '{0}'.".format(oid)
 
 # Insert a new member into the staff collection
 def create_member():
@@ -150,8 +174,9 @@ def prepare_json(ingredients):
 install_routes(bottle)
 
 # Run app using the default wsgiref
-if __name__ == "__main__":
+if __name__ == '__main__':
     sb = settings['bottle']
-    bottle.run(host=sb['host'], port=sb['port'], debug=sb['debug'], reloader=sb['reloader'])
+    bottle.run(host=sb['host'], port=sb['port'], debug=sb['debug'],
+               reloader=sb['reloader'])
 
 app = bottle.default_app()
