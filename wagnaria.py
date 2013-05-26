e12989508474bef677fb13140714f377771c8d2f
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2013-05-26 12:56:33 -0400

    Ensure PEP8 compliancy, add docstrings, remove (currently) unused functions.

--- wagnaria.py
+++ wagnaria.py
@@ -14,6 +14,7 @@ from bottle import HTTPError, request, response
 POSITIONS = ('translator', 'editor', 'timer', 'typesetter')
 
 class Wagnaria(object):
+    """ Main application that serves as a hypervisor to Bottle. """
     def __init__(self):
         # Default Settings. You can place overrides in config.yaml.
         # 'bottle_run' settings are only used when executed standalone.
@@ -29,7 +30,7 @@ class Wagnaria(object):
                 'port': 9002
             }
         }
-        # Update settings if a config file exists.
+        # Overwrite settings if a config file exists.
         try:
             cfg = open('config.yaml')
         except (FileNotFoundError, PermissionError) as e:
@@ -43,6 +44,7 @@ class Wagnaria(object):
                 exit(1)
             finally:
                 cfg.close()
+
         # Establish a MongoDB connection.
         try:
             self.client = MongoClient(self.config['mongo']['host'],
@@ -52,18 +54,24 @@ class Wagnaria(object):
             exit(1)
         # Load the database if it exists, create it if it doesn't.
         self.db = self.client[self.config['mongo']['name']]
+
+        # Initialize class objects for shows and staff.
         self.staff = StaffCollection(self.db.staff, self.db.shows)
         self.shows = ShowsCollection(self.db.shows, self.db.staff)
+
+        # Assign this object to the first app in Bottle's AppStack().
         self.app = bottle.default_app()
         # ObjectId Filter
         self.app.router.add_filter('oid', lambda x: (r'[0-9a-f]{24}', ObjectId, str))
+        # Setup the routes to be handled by this object.
         self.install_routes(self.app)
         # Assign most common "error" routes to a custom error handler.
         errors = {sc: self.error_page for sc in range(400, 415)}
         errors[500] = self.error_page
         self.app.error_handler = errors
-        # Run app using the default wsgiref
+
         if __name__ == '__main__':
+            # Run app using bottle_run settings if executed standalone.
             sb = []
             for k, v in self.config['bottle_run'].items():
                 if isinstance(v, str):
@@ -72,18 +80,18 @@ class Wagnaria(object):
                     sb.append("%s=%s" % (k, v))
             eval("bottle.default_app().run({0})".format(', '.join(sb)))
 
-    # Index page
     def index(self):
+        """ Return an index page. """
         return("<pre>Strawberries and cream, \nbiscuits and tea, \nwon't you "
                "join me \nin the oak tree?</pre>")
 
-    # Return JSON documents when the application returns an HTTPError
-    def error_page(self, error):
+    def error_page(self, e):
+        """ Return JSON documents when the application returns an HTTPError. """
         response.content_type = 'application/json'
-        return dumps({'status_code': error._status_code, 'message': error.body})
+        return dumps({'status_code': e._status_code, 'message': e.body})
 
-    # Pre-define routes and their respective functions
     def install_routes(self, b):
+        """ Define routes to their select functions for a Bottle app. """
         b.route('/', 'ANY', self.index)
         b.route('/shows', 'GET', self.shows.all_docs)
         b.route('/shows/ref', 'GET', self.shows.all_docs_short)
@@ -95,65 +103,57 @@ class Wagnaria(object):
         b.route('/staff/ref', 'GET', self.staff.all_docs_short)
         b.route('/staff/<oid:oid>', 'GET', self.staff.by_id)
         b.route('/staff/<oid:oid>/shows', 'GET', self.staff.show_history)
-"""    
-    app.route('/shows', ['POST'], create_show)
-    app.route('/shows/<oid:oid>', ['PUT'], modify_show)
-    app.route('/shows/<oid:oid>', ['DELETE'], destroy_show)
-    app.route('/staff', ['POST'], create_member)
-    app.route('/staff/<oid:oid>', ['PUT'], modify_member)
-    app.route('/staff/<oid:oid>', ['DELETE'], destroy_member)"""
 
 class RESTfulCollection(object):
+    """ Defines common web functions for use with MongoDB collections """
     def __init__(self, collection):
         self.collection = collection
+
     def reply(self, data):
+        """ Prepare a BSON dict to output as JSON in an HTTP response. """
         response.content_type = 'application/json'
         return dumps(data)
+
     def find_by_id(self, oid, projection=None):
+        """ Locate a document by ObjectId oid. """
         document = self.collection.find_one(oid, projection)
         if document:
             return document
         else:
             raise HTTPError(404, 'Could not find document %s.' % str(oid))
+
     def find(self, query=None, projection=None):
+        """ Locate documents, using a query if applicable. """
         documents = [d for d in self.collection.find(query, projection)]
         if documents:
             return documents
         else:
             raise HTTPError(404, 'No documents matched. Query: ' + vars(query))
+
     def all_docs(self):
+        """ Return all documents in the collection. """
         return self.reply(self.find())
+
     def by_id(self, oid):
+        """ Return a single document in the collection. """
         return self.reply(self.find_by_id(oid))
-    def create(self, document):
-        #show_data = request.json
-        try:
-            oid = self.insert(document)
-        except Exception as e:
-            return "Could not insert document. "+e
-    #def modify(self, oid, 
-    def destroy(self, oid):
-        try:
-            self.collection.find_one({'_id': oid})
-        except:
-            return "No"
-        try:
-            info = self.remove(oid, safe=True)
-        except:
-            return "Something went terribly wrong. "+e
-        return info
 
 class ShowsCollection(RESTfulCollection):
+    """ Extends RESTfulCollection for the shows collection """
     def __init__(self, shows_collection, staff_collection):
         self.collection = shows_collection
         self.staff = staff_collection
+
     def resolve_staff(self, show):
+        """ Cross-reference a show to update it with staff member names. """
         for position in POSITIONS:
             member = show['staff'][position]
             if 'name' not in member:
                 member['name'] = self.staff.find_one(member['id'])['name']
         return show
+
     def by_id(self, oid, key=None):
+        """ Return a show, or a show's key. """
         show = self.resolve_staff(self.find_by_id(oid))
         if key:
             for k in key.split('.'):
@@ -166,14 +166,20 @@ class ShowsCollection(RESTfulCollection):
             else:
                 raise HTTPError(404, "%s is undefined for this show." % key)
         return self.reply(show)
+
     def all_docs(self):
+        """ Return a list of all shows. """
         shows = self.find()
         shows = map(lambda s: self.resolve_staff(s), shows)
         return self.reply(shows)
+
     def all_docs_short(self):
+        """ Return a short list of shows with referential keys. """
         shows = self.find(None, {'titles': 1})
         return self.reply(shows)
+
     def by_group(self, group):
+        """ Return a specific list of shows with pre-defined filters. """
         query = {
             'complete': {"status": "complete"},
             'incomplete': 
@@ -189,8 +195,9 @@ class ShowsCollection(RESTfulCollection):
         shows = self.find(query)
         shows = map(lambda s: self.resolve_staff(s), shows)
         return self.reply(shows)
+
     def impute(self, oid):
-        # Return information about who's stalling a show
+        """ Return information about who's stalling a show. """
         show = self.resolve_staff(self.find_by_id(oid))
         p = show['progress']
         at = {'_id': oid}
@@ -218,14 +225,18 @@ class ShowsCollection(RESTfulCollection):
         return self.reply(at)
 
 class StaffCollection(RESTfulCollection):
+    """ Extends RESTfulCollection for the staff collection. """
     def __init__(self, staff_collection, shows_collection):
         self.collection = staff_collection
         self.shows = shows_collection
+
     def all_docs_short(self):
+        """ Return a short list of staff with referential keys. """
         staff = self.find(None, {'name': 1})
         return self.reply(staff)
+
     def show_history(self, oid):
-        # Return a list of shows a staff member has worked on
+        """ Return a list of shows a staff member has worked on. """
         query = [{'staff.%s.id' % p: oid} for p in POSITIONS]
         results = self.shows.find({'$or': query})
         shows = map(lambda s: s['titles']['english'], results)
